0050a67379adebc45da6c0d6315bc71cb72b0cec
RJ Aylward <rjaylwar@gmail.com>
2019-07-13 10:53:55 -0700

    removing in class code

--- app/src/main/java/com/ucsdextandroid2/petfinder/PetsActivity.kt
+++ app/src/main/java/com/ucsdextandroid2/petfinder/PetsActivity.kt
@@ -34,111 +34,11 @@ class PetsActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
-        val adapter = PetsAdapter()
+//        val adapter = PetsAdapter()
         recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recyclerView.adapter = adapter
+//        recyclerView.adapter = adapter
 
-        val petsLiveData = LivePagedListBuilder(PetsDataSourceFactory(null, null), PagedList.Config.Builder()
-            .setEnablePlaceholders(false)
-            .setPrefetchDistance(10)
-            .build()
-        ).build()
-
-        petsLiveData.observe(this, Observer<PagedList<PetModel>> { notes ->
-            adapter.submitList(notes)
-        })
-
-        checkLocationPermission()
-
-    }
-
-    private fun checkLocationPermission() {
-
-        // Here, thisActivity is the current activity
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 5)
-            }
-        } else {
-            // Permission has already been granted
-
-            getLocation()
-        }
-
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLocation() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
-            // Got last known location. In some rare situations this can be null.
-            Toast.makeText(this, location?.latitude.toString(), Toast.LENGTH_SHORT).show()
-        }.addOnFailureListener {
-            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        when (requestCode) {
-            7 -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    getLocation()
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return
-            }
-            else -> {
-                // Ignore all other requests.
-            }
-        }
-    }
-
-    private class PetsAdapter : PagedListAdapter<PetModel, PetCardViewHolder>(noteDiffer) {
-
-        var itemClickListener: ((PetModel) -> Unit)? = null
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetCardViewHolder {
-            return PetCardViewHolder.inflate(parent).apply {
-                this.itemView.setOnClickListener {
-                    val item = getItem(this.adapterPosition)
-                    if(item != null)
-                        itemClickListener?.invoke(item)
-                }
-            }
-        }
-
-        override fun onBindViewHolder(holder: PetCardViewHolder, position: Int) {
-            holder.bind(getItem(position))
-        }
-
-        companion object {
-            private val noteDiffer = object : DiffUtil.ItemCallback<PetModel>() {
-
-                override fun areItemsTheSame(oldItem: PetModel, newItem: PetModel): Boolean =
-                    oldItem.id == newItem.id
-
-                override fun areContentsTheSame(oldItem: PetModel, newItem: PetModel): Boolean =
-                    oldItem == newItem
-
-            }
-        }
+        //LivePagedListBuilder of the PetsDataSourceFactory
     }
 
     private class PetCardViewHolder private constructor(view: View) : RecyclerView.ViewHolder(view) {
@@ -156,7 +56,7 @@ class PetsActivity : AppCompatActivity() {
 
         fun bind(note: PetModel?) {
             image.isVisible = note?.imageUrl != null
-            image.loadImageUrl(note?.imageUrl)
+//            image.loadImageUrl(note?.imageUrl)
             titleView.text = note?.name
             textView.text = note?.breed
         }
--- app/src/main/java/com/ucsdextandroid2/petfinder/PicassoExt.kt
+++ app/src/main/java/com/ucsdextandroid2/petfinder/PicassoExt.kt
@@ -6,7 +6,3 @@ import com.squareup.picasso.Picasso
 /**
  * Created by rjaylward on 2019-07-13
  */
-
-fun ImageView.loadImageUrl(imageUrl: String?) {
-    Picasso.get().load(imageUrl).into(this)
-}
